1cdc68c073235a1caac759e34fd00d5a8fc645a3
namktra <namktr0201@gmail.com>
2026-02-03 10:36:38 -0500

    Comments for Realignment Programming

--- src/distance.cpp
+++ src/distance.cpp
@@ -8,20 +8,29 @@ double offsetSide = 0;
 double offsetBack = 0;
 
 void ReAlign(int quadrant){
+
+    // taking the readings from the sensors; 
+    // the offset is the distance from the sensor to middle of the bot ||  NEEDS CHANGING
     double sideReading = side.get() + offsetSide;
     double backReading = back.get() + offsetBack;
 
+
+    // takes the current position and saves them into variables
     Pose curr = chassis.getPose();
     double currHeading = curr.theta;
     double currX = curr.x;
     double currY = curr.y;
-
+    
+    // figuring out the orientation of the robot based on heading
     int direction;
     if(currHeading > 315 && currHeading <= 45){direction = 1;}
     if(currHeading > 45 && currHeading <= 135){direction = 2;}
     if(currHeading > 135 && currHeading <= 225){direction = 3;}
     if(currHeading > 225 && currHeading <= 315){direction = 4;}
 
+
+    // recalculating the true position of the robot on the field in relation 
+    // to its current quadrant and orientation (direction)
     if(quadrant == 1){
         
         if(direction == 1){chassis.setPose(currX, 0 - (72-backReading),currHeading);}
@@ -54,9 +63,14 @@ void ReAlign(int quadrant){
         
     }
 
+    // saving the new positions and printing them to the terminal to debug and check
+    curr = chassis.getPose();
+    currHeading = curr.theta;
+    currX = curr.x;
+    currY = curr.y;
 
-
-    
-
+    cout << currX << endl;
+    cout << currY << endl;
+    cout << currHeading << endl;
 }
 
